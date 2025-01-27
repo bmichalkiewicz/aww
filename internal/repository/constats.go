45581b5e5aaa18c4f381f67843ce729da93ae3a0
Bartosz Michałkiewicz <bartosz.michalkiewicz@outlook.com>
2025-01-28 00:29:23 +0100

    chore: easier regexp, more friedly code

--- internal/repository/constats.go
+++ internal/repository/constats.go
@@ -8,13 +8,8 @@ import (
 	"github.com/rs/zerolog/log"
 )
 
-// Define patterns for extracting components from the SSH URL
-var (
-	patterns = map[string]string{
-		"fqdn":    `git@([\w\.-]+):`, // Matches the FQDN
-		"folders": `:(.+)\.git$`,     // Matches folders
-	}
-)
+// Define pattern for extracting components from the SSH URL
+var pattern = `^git@([a-zA-Z0-9.-]+):([a-zA-Z0-9_./-]+)\.git$`
 
 type Group struct {
 	Name     string     `yaml:"name"`
@@ -43,34 +38,39 @@ func (p *Project) GetFolders() string {
 }
 
 func (p *Project) GetPath() string {
-	return filepath.Join(p.FQDN, p.Folders)
+	return filepath.Join(DestRepoPath, p.FQDN, p.Folders)
+}
+
+func (p *Project) Validate(url string) error {
+	matched, err := regexp.MatchString(pattern, url)
+	if err != nil {
+		return fmt.Errorf("error validating SSH URL: %v", err)
+	}
+	if !matched {
+		return fmt.Errorf("invalid SSH URL provided (example: git@gitlab.com:goodgroup/goodrepo.git)")
+	}
+
+	return nil
 }
 
 // Decode parses an SSH URL into a Project struct
 func (p *Project) Decode() error {
-	// Extract FQDN
-	fqdnRegexp := regexp.MustCompile(patterns["fqdn"])
-	fqdnMatch := fqdnRegexp.FindStringSubmatch(p.Url)
-	if len(fqdnMatch) < 2 {
-		return ErrInvalidUrl(fmt.Sprintf("FQDN not found in URL: %s", p.Url))
+	// Validate the SSH URL
+	err := p.Validate(p.Url)
+	if err != nil {
+		return err
 	}
-	p.FQDN = fqdnMatch[1]
 
-	// Extract Folders (or root folder if no subfolders exist)
-	foldersRegexp := regexp.MustCompile(patterns["folders"])
-	foldersMatch := foldersRegexp.FindStringSubmatch(p.Url)
-	if len(foldersMatch) < 2 {
-		return ErrInvalidUrl(fmt.Sprintf("Folders not found in URL: %s", p.Url))
+	regex := regexp.MustCompile(pattern)
+	submatches := regex.FindStringSubmatch(p.Url)
+
+	// Ensure the submatches contain the expected groups
+	if len(submatches) < 2 {
+		return fmt.Errorf("failed to extract groups from SSH URL")
 	}
-	p.Folders = foldersMatch[1]
 
+	p.FQDN = submatches[1]
+	p.Folders = submatches[2]
 	log.Debug().Str("service", "helpers").Str("folders", p.Folders).Str("fqdn", p.FQDN).Send()
 	return nil
-}
-
-// ErrInvalidUrl represents an error for invalid SSH URL parsing
-type ErrInvalidUrl string
-
-func (e ErrInvalidUrl) Error() string {
-	return string(e)
 }
